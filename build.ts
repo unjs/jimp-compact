c06d11d5f7eb97c01f4eff2bf42a27f4b21949fb
Pooya Parsa <pyapar@gmail.com>
2021-09-20 16:26:41 +0200

    chore: update build script with typescript

---
+++ build.ts
@@ -0,0 +1,34 @@
+import { resolve, dirname } from 'path'
+import { outputFile, readJSON, writeJSON } from 'fs-extra'
+import ncc from '@vercel/ncc'
+
+async function main() {
+  const input = resolve(__dirname, 'src', 'jimp.js')
+  const output = resolve(__dirname, 'dist', 'jimp.js')
+  const pkg = resolve(__dirname, 'package.json')
+
+  const opts = {
+    cache: false,
+    minify: true,
+    sourceMapRegister: false
+  }
+
+  // @ts-ignore
+  let { code } = await ncc(input, opts)
+
+  code = code.replace(/new Buffer/g, 'new JIMPBUffer')
+
+  await outputFile(output, code)
+
+  const jimpDir = dirname(require.resolve('jimp/package.json'))
+  const { version } = await readJSON(resolve(jimpDir, 'package.json'))
+
+  await writeJSON(pkg, { ...await readJSON(pkg), version }, { spaces: 2 })
+
+  console.log('jimp-compact@' + version)
+}
+
+main().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
